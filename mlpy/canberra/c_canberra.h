6637dff85222f2d9f798c32208bbf323188c9299
Davide Albanese <davide.albanese@gmail.com>
2011-10-11 16:41:29 +0200

    Initial release

---
+++ mlpy/canberra/c_canberra.h
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+
+double c_canberra(double *x, double *y, long n);
+double c_canberra_location(long *x, long *y, long n, long k);
+double c_canberra_stability(long *x, long n, long p, long k);
