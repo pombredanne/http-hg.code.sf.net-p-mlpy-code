f44ce7e57b4d901dfbe8dfd3bac024da33699f14
Davide Albanese <davide.albanese@gmail.com>
2011-12-21 13:48:00 +0100

    fix __init__.py

--- mlpy/__init__.py
+++ mlpy/__init__.py
@@ -12,7 +12,7 @@ if sys.version >= '3':
     from .kernel import *
     from .adatron import *
     from .canberra import *
-    from .dtw import *
+    #from .dtw import *
 else:
     import gsl
     from libsvm import *
@@ -23,7 +23,7 @@ else:
     from kernel import *
     from adatron import *
     from canberra import *
-    from dtw import *
+    #from dtw import *
 
 # python modules
 from crossval import *
@@ -103,4 +103,4 @@ __all__ += ['canberra', 'canberra_location', 'canberra_stability',
             'canberra_location_expected', 'canberra_location_max', 
             'canberra_stability_max']
 __all__ += ['KNN', 'ClassTree', 'MaximumLikelihoodC']
-__all__ += ['dtw']
+#__all__ += ['dtw']
